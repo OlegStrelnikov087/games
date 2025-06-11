75736628852ff2b4b3b34549880013ca9d3664eb
Oleg Strelnikov <olegstr08@gmail.com>
2025-06-11 21:51:19 +0400

    style(tic-tac-toe.css): перенести стили сохранив их как в проекте крестики-нолики

--- src/pages/tic-tac-toe/tic-tac-toe.tsx
+++ src/pages/tic-tac-toe/tic-tac-toe.tsx
@@ -1,8 +1,13 @@
 import { Board } from "../../applicatoins/tic-tac-toe/components/border/board"
+import './tic-tac-toe.css'
 export const TicTacToe = () => {
     return (
-       <>
-        <Board/>
-       </>
+        <div className="tic-tac-toe-page">
+            <header>Tic Tac Toe</header>
+            <main>
+                <Board />
+            </main>
+        </div>
+
     )
 }
