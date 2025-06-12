0ed16c905d6d4c7393cc8facbd47c2d22c115e3a
Oleg Strelnikov <olegstr08@gmail.com>
2025-06-12 16:47:06 +0400

    style(darts.css): написать стили мишени для адаптации под разные устройства

--- src/pages/darts/darts.tsx
+++ src/pages/darts/darts.tsx
@@ -1,14 +1,16 @@
+import './darts.css'
 import { Target } from "../../applicatoins/darts/components/target/target";
 export const Darts = () => {
     const handleScore = (points: number) => {
         console.log(`Вы набрали: ${points} очков`);
-        // Здесь можно добавить логику обработки очков
-      };
-    
-      return (
-        <div>
-          <h1>Дартс</h1>
-          <Target onHit={handleScore} />
+    };
+
+    return (
+        <div className='darts-game'>
+            <h1>Дартс</h1>
+            <div className="target-wrapper">
+                <Target onHit={handleScore} />
+            </div>
         </div>
-      );
+    );
 }
